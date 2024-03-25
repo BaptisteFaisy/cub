7f49b861c41c449db36ed609d3ac0b3634d60d1c
Baptiste <baptiste.faisy@gmail.com>
2024-03-25 21:05:06 +0100

    dd

---
+++ srcs/mapchecker.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mapchecker.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfaisy <bfaisy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 19:22:23 by bfaisy            #+#    #+#             */
+/*   Updated: 2024/03/25 20:13:31 by bfaisy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	checkmap_x(char **tab, int i, int j);
+int	checkmap_y(char **tab, int j);
+
+int	checkmap_check_wall(char **tab)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (tab[i])
+	{
+		while (tab[i][j])
+		{
+			if (tab[i][j] == '0')
+			{
+				if (checkmap_x(tab, i, j) == 1 && checkmap_y(tab, j) == 1)
+					return (1);
+				else
+					return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	checkmap_x(char **tab, int i, int j)
+{
+	while (tab[i][j])
+	{
+		if (tab[i][j] == '1' && tab[i][j + 1] == '\0')
+			return (1);
+		j++;
+	}
+	return (0);
+}
+
+int	checkmap_y(char **tab, int j)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i][j] != '1' && tab[i][j] != '\0')
+		i++;
+	if (tab[i][j] == '\0')
+		return (0);
+	i++;
+	while (tab[i][j] != '1' && tab[i][j] != '\0')
+		i++;
+	if (tab[i][j] == '\0')
+		return (0);
+	return (1);
+}
+
+int	verifie_cub(const char *string)
+{
+    int			len;
+    const char	*extension;
+    int 		ext_len;
+
+	len = ft_strlen(string);
+	extension = ".cub";
+	ext_len = ft_strlen(extension);
+
+    if (len >= ext_len)
+	{
+        if (ft_strcmp(string + len - ext_len, extension) == 0)
+            return 1;
+    }
+	return (0);
+}
+
+static int	check_input(char **tab)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (tab[i])
+	{
+		while (tab[i][j])
+		{
+			if ()
+			j++;
+		}
+		i++;
+	}
+}
