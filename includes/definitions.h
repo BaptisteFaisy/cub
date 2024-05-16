4980cdb5dcf3ae89ee5e1aebc97e8e93140ae575
Hojoon LEE <joonlee031@live.com>
2024-05-17 01:18:01 +0200

    vsy

--- includes/definitions.h
+++ includes/definitions.h
@@ -6,7 +6,7 @@
 /*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 11:50:23 by lhojoon           #+#    #+#             */
-/*   Updated: 2024/05/17 01:01:30 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/05/17 01:17:17 by lhojoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@
 # define DEF_PLAYER_ROTATE_SPD 0.025
 # define DEF_DISTANCE_COEFF 0.005
 # define DEF_HEIGHT_COEFF 5000
-# define DEF_FOV_COEFF 0.005
-// # define DEF_CAMERA_CHECK_STRICT
+# define DEF_FOV_COEFF 0.003
+# define DEF_CAMERA_CHECK_STRICT
 
 int				verifie_cub(const char *string);
 int				check_input(char **tab);
