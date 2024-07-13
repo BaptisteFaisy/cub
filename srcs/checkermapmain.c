5a11c9f8db8c32f523c6e4f16427550dc1e781ca
Baptiste <baptiste.faisy@gmail.com>
2024-07-13 16:00:41 +0200

    dd

--- srcs/checkermapmain.c
+++ srcs/checkermapmain.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checkermapmain.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lhojoon <lhojoon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bfaisy <bfaisy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 11:20:09 by bfaisy            #+#    #+#             */
-/*   Updated: 2024/07/13 15:55:30 by lhojoon          ###   ########.fr       */
+/*   Updated: 2024/07/13 16:00:24 by bfaisy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 static int	check_double(char **tab);
 
-int	mapcheck_main(t_map_data *data)
+int	mapcheck_main(t_map_data *data, t_tab *head)
 {
 	if (!check_double(data->map) || !check_input(data->map)
-		|| !checkmap_check_wall(data->map) || !checklignemain(data->pref, t_tab *head))
+		|| !checkmap_check_wall(data->map) || !checklignemain(data->pref, head)) // ici faut changer
 		return (0);
 	return (1);
 }
